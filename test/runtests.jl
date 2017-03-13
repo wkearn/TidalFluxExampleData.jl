8c4bac2183da6c5a817d710536396d737734b775
Will Kearney <wkearn@gmail.com>
2017-03-13 15:14:35 -0400

    Fix tests for two deployments

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,16 @@
 using TidalDischargeModels, TidalFluxExampleData
 using Base.Test
 
-n = 6037 # Length of the Sweeney data
+n = 6037 # Length of the first Sweeney data
 
 creek = Creek{:sweeney}()
 deps = parse_deps(creek)
 
 # Figure out how platform independent this is
 @test hash(deps[1]) == 0x9075efca49bb29bb
+@test hash(deps[2]) == 0x6ddb46623bd6c559
 
-@test length(deps) == 1
+@test length(deps) == 2
 @test deps[1].location == creek
 @test deps[1].startDate == DateTime(2015,07,08,12,00,00)
 @test deps[1].endDate == DateTime(2015,08,19,10,00,00)
@@ -24,7 +25,7 @@ deps = parse_deps(creek)
 
 adata = load_data.(deps)
 
-@test length(adata) == 1
+@test length(adata) == 2
 @test length(adata[1].p) == n
 
 cs = parse_cs(creek)
