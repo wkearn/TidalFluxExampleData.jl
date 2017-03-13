75b3e5967c0d6bad14c8e7ebcd33bc7ce3738bdf
Will Kearney <wkearn@gmail.com>
2017-03-13 14:55:05 -0400

    Set the TDM data directory

--- src/TidalFluxExampleData.jl
+++ src/TidalFluxExampleData.jl
@@ -2,4 +2,7 @@ module TidalFluxExampleData
 
 using TidalDischargeModels
 
+setADCPdatadir!(Pkg.dir("TidalFluxExampleData","data","adcp"))
+setmetdatadir!(Pkg.dir("TidalFluxExampleData","data","met"))
+
 end # module
