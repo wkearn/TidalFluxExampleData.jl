7377234116ee30037e33d9dd2a5de7b4571e09b9
William Kearney <wkearn@gmail.com>
2017-03-14 18:37:33 -0400

    Remove dependencies on other TF packages.
    This means we can use TFED to test lower-level packages.
    Just be careful to set the datadirs when you use it.

--- src/TidalFluxExampleData.jl
+++ src/TidalFluxExampleData.jl
@@ -1,8 +1,3 @@
 module TidalFluxExampleData
 
-using PIEMetData, ADCPDataProcessing
-
-setADCPdatadir!(Pkg.dir("TidalFluxExampleData","data","adcp"))
-setmetdatadir!(Pkg.dir("TidalFluxExampleData","data","met"))
-
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,5 @@
-using DischargeData, PIEMetData, ADCPDataProcessing, TidalFluxExampleData
+using TidalFluxExampleData
 using Base.Test
 
-n = 6037 # Length of the first Sweeney data
-
-creek = Creek{:sweeney}()
-deps = parse_deps(creek)
-
-@test length(deps) == 2
-@test deps[1].location == creek
-@test deps[1].startDate == DateTime(2015,07,08,12,00,00)
-@test deps[1].endDate == DateTime(2015,08,19,10,00,00)
-
-@test deps[1].adcp.serialNumber == "AQD 5062"
-@test deps[1].adcp.hasAnalog == false
-@test deps[1].adcp.blankingDistance == 0.1113778
-@test deps[1].adcp.cellSize == 0.1999389
-@test deps[1].adcp.nCells == 40
-@test deps[1].adcp.deltaT == 600.0
-@test deps[1].adcp.elevation == -1.588
-
-adata = load_data.(deps)
-
-@test length(adata) == 2
-@test length(adata[1].p) == n
-
-cs = parse_cs(creek)
-
-@test cs.lmax == 13.944
-@test cs.Amax == 20.674
-@test cs.hmax == 0.35
-
-csd = load_data(cs)
-
-Q = Discharge(adata[1],csd)
+@test in("adcp",readdir(Pkg.dir("TidalFluxExampleData","data")))
+@test in("met",readdir(Pkg.dir("TidalFluxExampleData","data")))
